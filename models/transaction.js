cc23330c301352b8313761ab99fb282a33afc46c
lauren levaccare <laurenlevaccare@icloud.com>
2021-02-16 12:26:36 -0600

    deploying

---
+++ models/transaction.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter a name for transaction"
+    },
+    value: {
+      type: Number,
+      required: "Enter an amount"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+);
+
+const dbTransaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = dbTransaction;
--- server.js
+++ server.js
@@ -17,10 +17,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // mongo.db connection installed
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // install api routes
 app.use(require("./routes/api.js"));
